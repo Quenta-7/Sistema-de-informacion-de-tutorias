7b9edd405f7b682e85706faf38190a319e21b278
RichardPumaC <161809@unsaac.edu.pe>
2025-11-28 10:04:25 -0500

    Agregar prueba de Selenium para los elementos de inicio de sesión

---
+++ tests/selenium/test_elements_login.py
@@ -0,0 +1,19 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+import time
+
+service = Service("chromedriver.exe")
+driver = webdriver.Chrome(service=service)
+
+driver.get("http://localhost:5500/frontend/login.html")
+time.sleep(1)
+
+usuario = driver.find_element(By.XPATH, "//input[@type='text']")
+password = driver.find_element(By.XPATH, "//input[@type='password']")
+boton = driver.find_element(By.TAG_NAME, "button")
+
+print("Elementos encontrados correctamente.")
+
+time.sleep(1)
+driver.quit()
